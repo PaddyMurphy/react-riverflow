4bc565ce6bd01022c4fca0099fc44571d31f1e1a
PaddyMurphy <patrickblewis@gmail.com>
2017-06-23 11:02:38 -0500

    separate out tools

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Tools from './components/Tools/Tools';
 import Footer from './components/Footer/Footer';
 import Gridtable from './components/Gridtable/Gridtable';
 import './App.css';
@@ -16,34 +17,8 @@ class App extends Component {
 
           <section className="section">
             <div className="container">
-              <div className="notification is-danger is-hidden">
-                <button className="delete"></button>
-              </div>
 
-              <div className="notification content">
-                <a className="delete is-small"> </a>
-                <p>Riverflow provides the latest <abbr title="cubic feet per second">CFS</abbr> from the USGS gauges of floatable rivers and creeks. The color indicates optimal floating conditions with additional inforamtion and a 7 day graph in the details.</p>
-              </div>
-
-              <div className="columns is-flex tools">
-
-                <div className="column column-search">
-                  <div className="field level-item">
-                    <label htmlFor="search" className="label">Search</label>
-                    <p className="control">
-                      <input id="search" name="search" className="input" type="text" placeholder="Filter the table" />
-                      <a className="delete is-small"> </a>
-                    </p>
-                  </div>
-                </div>
-
-                <div className="column column-button">
-                  <button className="button is-primary">
-                    <span className="refresh-long is-hidden-mobile">refresh river table</span>
-                    <span className="refresh-short is-hidden-tablet">&#8634;</span>
-                  </button>
-                </div>
-              </div>
+              <Tools />
 
               <Gridtable />
 
---
+++ src/components/Tools/Tools.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './Tools.css';
+
+class Tools extends Component {
+
+  render() {
+    return (
+      <div className="Tools">
+        <div className="notification is-danger is-hidden">
+          <button className="delete"></button>
+        </div>
+
+        <div className="notification content">
+          <a className="delete is-small"> </a>
+          <p>Riverflow provides the latest <abbr title="cubic feet per second">CFS</abbr> from the USGS gauges of floatable rivers and creeks. The color indicates optimal floating conditions with additional inforamtion and a 7 day graph in the details.</p>
+        </div>
+
+        <div className="columns is-flex tools">
+
+          <div className="column column-search">
+            <div className="field level-item">
+              <label htmlFor="search" className="label">Search</label>
+              <p className="control">
+                <input id="search" name="search" className="input" type="text" placeholder="Filter the table" />
+                <a className="delete is-small"> </a>
+              </p>
+            </div>
+          </div>
+
+          <div className="column column-button">
+            <button className="button is-primary">
+              <span className="refresh-long is-hidden-mobile">refresh river table</span>
+              <span className="refresh-short is-hidden-tablet">&#8634;</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Tools;
