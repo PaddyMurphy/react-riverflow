60efb7f1ed835bc5675e508e7683b617e653c9c7
PaddyMurphy <patrickblewis@gmail.com>
2017-06-26 16:35:44 -0500

    correctly compose components and props/state

--- src/components/Gridtable/Gridtable.js
+++ src/components/Gridtable/Gridtable.js
@@ -1,54 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import Classnames from 'classnames'
-import Axios from 'axios';
 import Gridrow from './Gridrow';
-import Rivers from '../../rivers.json';
-import Conditions from '../../conditions.json';
 import './Gridtable.css';
-
-const baseMapUrl = '//maps.google.com/?q=';
-const baseUsgsUrl = 'https://waterservices.usgs.gov/nwis/iv/';
-const graphType = '00060'; // defaults to cfs
-const rivers = Rivers.data;
-let riversFormatted = [];
 
 class Gridtable extends Component {
 
-  constructor(props) {
-    super(props);
-
-    // let sortOrderItems = {}
-    // this.columns.forEach(function (key) {
-    //   sortOrderItems[key] = 1
-    // })
-
-    this.state = {
-      error: undefined,
-      loading: false,
-      showLegend: true,
-      searchQuery: '',
-      tableData: undefined
-    }
-
-    this.sites = this.formatSites();
-
+  static propTypes = {
+    error: PropTypes.bool,
+    graphType: PropTypes.string,
+    loading: PropTypes.bool,
+    tableData: PropTypes.array
   }
 
+  // constructor(props) {
+  //   super(props);
+  // }
+
   render() {
-    // set data here
     const vm = this;
     let rows = [];
-    let data = this.state.tableData;
+    let data = this.props.tableData;
 
     let loadingClasses = Classnames(
       'loading notification is-warning', {
-      'is-hidden': this.state.loading ? false : true
+      'is-hidden': this.props.loading ? false : true
       }
     )
 
     let tableClasses = Classnames(
       'table', {
-      'invisible': this.state.loading ? true : false
+      'invisible': this.props.loading ? true : false
       }
     )
 
@@ -57,8 +39,8 @@ class Gridtable extends Component {
         rows.push(<Gridrow
           key={row.site}
           tableData={row}
-          loading={vm.loading}
-          graphType={graphType}
+          loading={vm.props.loading}
+          graphType={vm.props.graphType}
         />)
       })
     }
@@ -100,189 +82,6 @@ class Gridtable extends Component {
         </table>
       </div>
     );
-  }
-
-  componentDidMount() {
-    // load the river data
-    this.getUsgsData();
-  }
-
-  formatSites() {
-    let list = [];
-
-    rivers.forEach(function (d) {
-      // return only number values
-      if (d.value.match(/\d+/g)) {
-        list.push(d.value);
-      }
-    });
-
-    return list.join();
-  }
-
-  /**
-   * Fetches usgs instant data from rivers.json.
-   * @return {number[]} response
-   */
-  getUsgsData() {
-    var vm = this;
-
-    riversFormatted = [];
-    vm.setState({loading: true});
-    // fetch all site numbers in rivers.json
-    Axios.get(baseUsgsUrl, {
-      params: {
-        parameterCd: graphType,
-        sites: this.sites,
-        format: 'json',
-        period: 'PT12H', // past 12 hours
-        siteStatus: 'active'
-      }
-    })
-    .then(response => {
-      vm.setState({loading: false});
-      if (response.data.value.timeSeries) {
-        vm.displayUsgsData(response.data.value.timeSeries);
-        vm.setState({error: ''});
-      } else {
-        vm.setState({error: 'no river data available'});
-      }
-    })
-    .catch(error => {
-      console.log(error);
-      vm.setState({loading: false});
-      vm.setState({error: error.message});
-    });
-  }
-
-  /**
-   * Formats usgs response for display from rivers.json.
-   * @return {number[]} formatted response
-   * @param {number[]} response - usgs fetch response.
-   */
-  displayUsgsData(response) {
-    const vm = this;
-    const today = new Date();
-    let arr;
-    let river = {};
-    let currentValue;
-    let date;
-    let geo;
-    let itemsProcessed = 0;
-    let oldestValue;
-    let newestValue;
-    let percentChanged;
-    let rising;
-    let risingFast;
-    let risingFastThreshold = 130; // percent change
-    let site;
-    let time;
-
-    response.forEach(function (d, i, a) {
-      // NOTE: some rivers do not support cfs (00060)
-      arr = d.values[0].value;
-      // return on error
-      if (!arr[0]) return;
-
-      // oldestValue is the first item
-      oldestValue = parseInt(arr[0].value, 10);
-      // currentValue is the last item
-      currentValue = arr[arr.length - 1];
-      newestValue = parseInt(currentValue.value, 10);
-      // get current date / time
-      date = new Date(currentValue.dateTime);
-      time = date.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: false});
-
-      percentChanged = Math.round((newestValue / oldestValue) * 100);
-
-      // only show date if not today
-      if (today.toDateString() === date.toDateString()) {
-        date = '';
-      }
-
-      geo = d.sourceInfo.geoLocation.geogLocation;
-      site = d.sourceInfo.siteCode[0].value;
-      rising = (newestValue > oldestValue);
-      risingFast = (percentChanged > risingFastThreshold);
-
-      river = {
-        'name': d.sourceInfo.siteName,
-        'location': baseMapUrl + geo.latitude + ',+' + geo.longitude,
-        'site': site,
-        'date': date,
-        'time': time,
-        'cfs': newestValue,
-        'oldCfs': oldestValue,
-        'condition': vm.getConditions(newestValue).condition,
-        'level': vm.getConditions(newestValue).level,
-        'rising': rising,
-        'risingFast': risingFast
-      }
-      // TODO: add back merge with ww class
-      // vm.mergeRiverInfo(river);
-      riversFormatted.push(river);
-
-      itemsProcessed++;
-
-      if(itemsProcessed === a.length) {
-        vm.setState({tableData: riversFormatted});
-      }
-    });
-  }
-  /**
-   * Merges class from rivers.json to matching response
-   * matches are based on USGS site numbers
-   * @param {Object} river
-   */
-  // mergeRiverInfo(river) {
-  //   const vm = this;
-  //   const currentRiver = river.site;
-
-  //   vm.rivers.forEach(function (d) {
-  //     // add white water class
-  //     if (d.value === currentRiver) {
-  //       river.class = d.class;
-  //     }
-  //   });
-
-  //   vm.riversFormatted.push(river);
-  // }
-
-  /**
-   * Returns condition description and level color code from conditions.json
-   * @return {Object} condition and level
-   * @param {number} cfs - from usgs fetch response.
-   */
-  getConditions(cfs) {
-    let condition;
-    let level;
-    // convert to number
-    cfs = parseInt(cfs, 10);
-    // check the range of the cfs and display the appropriate message
-    if (cfs === 0) {
-      condition = Conditions.flow0;
-      level = 'level-0'
-    } else if ((cfs > 0) && (cfs < 50)) {
-      condition = Conditions.flow1;
-      level = 'level-1'
-    } else if ((cfs >= 50) && (cfs < 100)) {
-      condition = Conditions.flow2;
-      level = 'level-2'
-    } else if ((cfs >= 100) && (cfs < 300)) {
-      condition = Conditions.flow3;
-      level = 'level-3'
-    } else if ((cfs >= 300) && (cfs < 600)) {
-      condition = Conditions.flow4;
-      level = 'level-4'
-    } else if ((cfs >= 600) && (cfs < 2000)) {
-      condition = Conditions.flow5;
-      level = 'level-5'
-    } else if (cfs >= 2000) {
-      condition = Conditions.flow6;
-      level = 'level-6'
-    }
-
-    return {'condition': condition, 'level': level};
   }
 }
 
--- src/components/Riverflow/Riverflow.js
+++ src/components/Riverflow/Riverflow.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
-import './Riverflow.css';
+import Axios from 'axios';
+import Classnames from 'classnames';
 import Gridtable from '../../components/Gridtable/Gridtable';
+import Rivers from '../../rivers.json';
+import Conditions from '../../conditions.json';
+import './Riverflow.css';
+
+const baseMapUrl = '//maps.google.com/?q=';
+const baseUsgsUrl = 'https://waterservices.usgs.gov/nwis/iv/';
+const rivers = Rivers.data;
+let riversFormatted = [];
 
 class Riverflow extends Component {
 
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: false,
+      graphType: '00060', // defaults to cfs
+      loading: false,
+      searchQuery: '',
+      tableData: []
+    }
+
+    this.sites = this.formatSites();
+  }
 
   // bind this
-  handleClick = (e) => {
-    console.log('this is:', this);
-    console.log('event ', e);
+  refreshTable = (e) => {
+    e.preventDefault();
+    this.getUsgsData();
   }
 
   render() {
+    let refreshClasses = Classnames(
+      'button is-primary', {
+      'is-loading': this.state.loading
+      }
+    )
+
     return (
       <div className="rivertable">
         <section className="section">
@@ -42,7 +67,7 @@ class Riverflow extends Component {
                 </div>
 
                 <div className="column column-button">
-                  <button className="button is-primary" onClick={this.handleClick}>
+                  <button className={refreshClasses} onClick={this.refreshTable}>
                     <span className="refresh-long is-hidden-mobile">refresh river table</span>
                     <span className="refresh-short is-hidden-tablet">&#8634;</span>
                   </button>
@@ -50,12 +75,197 @@ class Riverflow extends Component {
               </div>
             </div>
 
-            <Gridtable />
+            <Gridtable
+              loading={this.state.loading}
+              tableData={this.state.tableData}
+              graphType={this.state.graphType}
+            />
 
           </div>
         </section>
       </div>
     );
+  }
+
+  componentDidMount() {
+    // load the river data
+    this.getUsgsData();
+  }
+
+  formatSites() {
+    let list = [];
+
+    rivers.forEach(function (d) {
+      // return only number values
+      if (d.value.match(/\d+/g)) {
+        list.push(d.value);
+      }
+    });
+
+    return list.join();
+  }
+
+  /**
+   * Fetches usgs instant data from rivers.json.
+   * @return {number[]} response
+   */
+  getUsgsData() {
+    riversFormatted = [];
+    this.setState({loading: true});
+    // fetch all site numbers in rivers.json
+    Axios.get(baseUsgsUrl, {
+      params: {
+        parameterCd: this.state.graphType,
+        sites: this.sites,
+        format: 'json',
+        period: 'PT12H', // past 12 hours
+        siteStatus: 'active'
+      }
+    })
+    .then(response => {
+      this.setState({loading: false});
+      if (response.data.value.timeSeries) {
+        this.displayUsgsData(response.data.value.timeSeries);
+        this.setState({error: ''});
+      } else {
+        this.setState({error: 'no river data available'});
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      this.setState({loading: false});
+      this.setState({error: error.message});
+    });
+  }
+
+  /**
+   * Formats usgs response for display from rivers.json.
+   * @return {number[]} formatted response
+   * @param {number[]} response - usgs fetch response.
+   */
+  displayUsgsData(response) {
+    const vm = this;
+    const today = new Date();
+    let arr;
+    let river = {};
+    let currentValue;
+    let date;
+    let geo;
+    let itemsProcessed = 0;
+    let oldestValue;
+    let newestValue;
+    let percentChanged;
+    let rising;
+    let risingFast;
+    let risingFastThreshold = 130; // percent change
+    let site;
+    let time;
+
+    response.forEach(function (d, i, a) {
+      // NOTE: some rivers do not support cfs (00060)
+      arr = d.values[0].value;
+      // return on error
+      if (!arr[0]) return;
+
+      // oldestValue is the first item
+      oldestValue = parseInt(arr[0].value, 10);
+      // currentValue is the last item
+      currentValue = arr[arr.length - 1];
+      newestValue = parseInt(currentValue.value, 10);
+      // get current date / time
+      date = new Date(currentValue.dateTime);
+      time = date.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: false});
+
+      percentChanged = Math.round((newestValue / oldestValue) * 100);
+
+      // only show date if not today
+      if (today.toDateString() === date.toDateString()) {
+        date = '';
+      }
+
+      geo = d.sourceInfo.geoLocation.geogLocation;
+      site = d.sourceInfo.siteCode[0].value;
+      rising = (newestValue > oldestValue);
+      risingFast = (percentChanged > risingFastThreshold);
+
+      river = {
+        'name': d.sourceInfo.siteName,
+        'location': baseMapUrl + geo.latitude + ',+' + geo.longitude,
+        'site': site,
+        'date': date,
+        'time': time,
+        'cfs': newestValue,
+        'oldCfs': oldestValue,
+        'condition': vm.getConditions(newestValue).condition,
+        'level': vm.getConditions(newestValue).level,
+        'rising': rising,
+        'risingFast': risingFast
+      }
+      // TODO: add back merge with ww class
+      // vm.mergeRiverInfo(river);
+      riversFormatted.push(river);
+
+      itemsProcessed++;
+
+      if(itemsProcessed === a.length) {
+        vm.setState({tableData: riversFormatted});
+      }
+    });
+  }
+  /**
+   * Merges class from rivers.json to matching response
+   * matches are based on USGS site numbers
+   * @param {Object} river
+   */
+  // mergeRiverInfo(river) {
+  //   const vm = this;
+  //   const currentRiver = river.site;
+
+  //   vm.rivers.forEach(function (d) {
+  //     // add white water class
+  //     if (d.value === currentRiver) {
+  //       river.class = d.class;
+  //     }
+  //   });
+
+  //   vm.riversFormatted.push(river);
+  // }
+
+  /**
+   * Returns condition description and level color code from conditions.json
+   * @return {Object} condition and level
+   * @param {number} cfs - from usgs fetch response.
+   */
+  getConditions(cfs) {
+    let condition;
+    let level;
+    // convert to number
+    cfs = parseInt(cfs, 10);
+    // check the range of the cfs and display the appropriate message
+    if (cfs === 0) {
+      condition = Conditions.flow0;
+      level = 'level-0'
+    } else if ((cfs > 0) && (cfs < 50)) {
+      condition = Conditions.flow1;
+      level = 'level-1'
+    } else if ((cfs >= 50) && (cfs < 100)) {
+      condition = Conditions.flow2;
+      level = 'level-2'
+    } else if ((cfs >= 100) && (cfs < 300)) {
+      condition = Conditions.flow3;
+      level = 'level-3'
+    } else if ((cfs >= 300) && (cfs < 600)) {
+      condition = Conditions.flow4;
+      level = 'level-4'
+    } else if ((cfs >= 600) && (cfs < 2000)) {
+      condition = Conditions.flow5;
+      level = 'level-5'
+    } else if (cfs >= 2000) {
+      condition = Conditions.flow6;
+      level = 'level-6'
+    }
+
+    return {'condition': condition, 'level': level};
   }
 }
 
