b0a048f00d09feddb319874faef8e28998bb54aa
plewis <plewis@indeed.com>
2018-12-27 19:01:41 -0600

    Updates

---
+++ precache-manifest.f4ff2f84c86d59b6c1780bee1b5ebf0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37cf6b9a52d82e45d245",
+    "url": "/react-riverflow/static/js/runtime~main.37cf6b9a.js"
+  },
+  {
+    "revision": "eb8ca4c3c75f45a4dc28",
+    "url": "/react-riverflow/static/js/main.eb8ca4c3.chunk.js"
+  },
+  {
+    "revision": "f7a12cccb917dbebd263",
+    "url": "/react-riverflow/static/js/1.f7a12ccc.chunk.js"
+  },
+  {
+    "revision": "eb8ca4c3c75f45a4dc28",
+    "url": "/react-riverflow/static/css/main.d4f6b6ec.chunk.css"
+  },
+  {
+    "revision": "a4ec8f1bd8f5aeadcca9ac070a7cfeac",
+    "url": "/react-riverflow/index.html"
+  }
+];
